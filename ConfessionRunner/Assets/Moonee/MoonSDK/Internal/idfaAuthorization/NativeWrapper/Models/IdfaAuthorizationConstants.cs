da24efddc25a01ffeab5e0a040d97f33934a79c4
enesnacaroglugm <enes18812009@hotmail.com>
2022-11-22 17:34:24 +0300

    clearOfDMCA

--- ConfessionRunner/Assets/Moonee/MoonSDK/Internal/idfaAuthorization/NativeWrapper/Models/IdfaAuthorizationConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Moonee.MoonSDK.Internal.IdfaAuthorization
-{
-    public class IdfaAuthorizationConstants
-    {
-        public const string AppTrackingTransparencySymbol = "VS_ATT";
-        public const string WithAttFolder = "Assets/Moonee/MooneeSDK/Internal/idfaAuthorization/NativeWrapper/Plugins/iOS/WithATT";
-        public const string UserTrackingUsageDescription = "Your data may be used by our team and our partners for personalized advertising and analytics purposes.";
-    }
-}
